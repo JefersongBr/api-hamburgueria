5f539f1cba3d1f98528cc05092f0028619e9b798
JefersongBr <micaelcarvalhopds@gmail.com>
2026-04-23 23:42:22 -0300

    feat: adiciona rota de produtos

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,43 @@
+﻿using APIHamburgueria.Context;
+using APIHamburgueria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIHamburgueria.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+
+    public class ProdutosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            try
+            {
+                var produtos = _context.Produtos.AsNoTracking().ToList();
+
+                if(produtos is null)
+                {
+                    return NotFound("Produtos não encontrado...");
+                }
+
+                return Ok(produtos);
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "ocorreu um erro ao tratar sua solicitação");
+            }
+        }
+
+    }
+}
